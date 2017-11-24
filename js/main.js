77b7f4d6756735b67ee20a1ff700e96be16cb882
slyty7 <tyler.j.west.7@gmail.com>
2017-11-24 11:48:41 -0800

    add graph

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const poundOutput = $(".gbp");
 
 function getStats(){
 	$.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
-		console.log(data);
 
 		//save current prices of bitcoin in us dollars, british pounds, and euros
 		let dollarPrice = data.bpi.USD.rate;
@@ -27,3 +26,58 @@ getStats();
 
 //update price every 30 seconds
 setTimeout(getStats, 30000)
+
+
+
+function getHistory(){
+	//reset dates and rates arrays
+	let dates = [];
+	let rates = [];
+
+	//get the rate history from last 30 days
+	$.getJSON('https://api.coindesk.com/v1/bpi/historical/close.json', function(data){
+		for (prop in data.bpi) {
+			var date = prop;
+			var rate = data.bpi[prop];
+
+			//add last 30 days rates to empty arrays
+			dates.push(date);
+			rates.push(rate);
+		}
+
+
+		//the chart....maybe
+		var ctx = $("#graph");
+		var myChart = new Chart(ctx, {
+		    type: 'line',
+		    data: {
+		        labels: dates,
+		        datasets: [{
+		            label: 'Rate in USD',
+		            fill: true,
+		            data: rates,
+		            backgroundColor: 'rgba(197, 248, 211, .5)',
+		            borderColor: '#50b767',
+		            borderWidth: 3
+		        }]
+		    },
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero:true
+		                }
+		            }]
+		        },
+		        elements: {
+		            line: {
+		                tension: 0, // disables bezier curves
+		            }
+		        }
+		    }
+		});
+	});
+}
+getHistory();
+
+
