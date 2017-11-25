03f5318cf2d7efd6874bd2b956dea3e775dbb189
slyty7 <tyler.j.west.7@gmail.com>
2017-11-24 18:13:30 -0800

    switch to just USD

--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
-const dollarOutput = $(".usd");
-const euroOutput = $(".eur");
-const poundOutput = $(".gbp");
 
 function getStats(){
 	$.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
 
-		//save current prices of bitcoin in us dollars, british pounds, and euros
+		//save current price of bitcoin
 		let dollarPrice = data.bpi.USD.rate;
-		let euroPrice = data.bpi.EUR.rate;
-		let poundPrice = data.bpi.GBP.rate;
 
 		//trim off excess decimal points
 		dollarPrice = dollarPrice.slice(0,-2);
-		euroPrice = euroPrice.slice(0,-2);
-		poundPrice = poundPrice.slice(0,-2);
-
-		//output converted rate of BitCoin
-		dollarOutput.text("\u0024" + dollarPrice);
-		euroOutput.text("\u20AC" + euroPrice);
-		poundOutput.text("\u00A3" + poundPrice);		
 	})
 };
 
@@ -30,21 +18,26 @@ setTimeout(getStats, 30000)
 
 
 function getHistory(){
-	//reset dates and rates arrays
-	let dates = [];
-	let rates = [];
+
 
 	//get the rate history from last 30 days
 	$.getJSON('https://api.coindesk.com/v1/bpi/historical/close.json', function(data){
+		//reset dates and rates arrays
+		let dates = [];
+		let rates = [];
+
+		//loop through past rates object
 		for (prop in data.bpi) {
-			var date = prop;
-			var rate = data.bpi[prop];
+			let date = prop;
+			let rate = data.bpi[prop];
 
 			//add last 30 days rates to empty arrays
 			dates.push(date);
 			rates.push(rate);
 		}
 
+		//find minimum value and sets y-axis on chart accordingly
+		let min = Math.floor(Math.min(...rates)) - 100;
 
 		//the chart....maybe
 		var ctx = $("#graph");
@@ -65,7 +58,7 @@ function getHistory(){
 		        scales: {
 		            yAxes: [{
 		                ticks: {
-		                    beginAtZero:true
+		                	suggestedMin: min
 		                }
 		            }]
 		        },
@@ -79,5 +72,3 @@ function getHistory(){
 	});
 }
 getHistory();
-
-
