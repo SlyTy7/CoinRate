f99adffbed695a2af440b7f1491a284b8698665a
slyty7 <tyler.j.west.7@gmail.com>
2017-11-24 19:09:56 -0800

    add 30 day change

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@
 function getStats(){
 	$.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
 
+		const currentDollars = $("#current-rate");
+		const currentCents = $("#current-cents");
+
 		//save current price of bitcoin
 		let dollarPrice = data.bpi.USD.rate;
+		let justDollars = dollarPrice.slice(0,-5);
+		let justCents = "." + dollarPrice.split(".")[1].slice(0,2);
 
-		//trim off excess decimal points
-		dollarPrice = dollarPrice.slice(0,-2);
+		currentDollars.html(justDollars);
+		currentCents.html(justCents);
+
 	})
 };
 
@@ -36,12 +42,20 @@ function getHistory(){
 			rates.push(rate);
 		}
 
+		let monthDiff = (rates[rates.length-1] - rates[0]).toString();
+		let dollarDiff = monthDiff.split(".")[0];
+		let centsDiff = "." + monthDiff.split(".")[1].slice(0,2);
+
+		//add change amount to html
+		$("#rate-change").html(dollarDiff);
+		$("#changed-cents").html(centsDiff);
+
 		//find minimum value and sets y-axis on chart accordingly
 		let min = Math.floor(Math.min(...rates)) - 100;
 
 		//the chart....maybe
-		var ctx = $("#graph");
-		var myChart = new Chart(ctx, {
+		let ctx = $("#graph");
+		let myChart = new Chart(ctx, {
 		    type: 'line',
 		    data: {
 		        labels: dates,
