583da48d0ee5a4d141464404a4f57d3bd58ec19b
slyty7 <tyler.j.west.7@gmail.com>
2017-11-27 18:35:27 -0800

    add vertical line on hover

--- js/main.js
+++ js/main.js
@@ -65,10 +65,37 @@ function getHistory(){
 		//find minimum value and sets y-axis on chart accordingly
 		let min = Math.floor(Math.min(...rates)) - 100;
 
+
+		Chart.defaults.LineWithLine = Chart.defaults.line;
+		Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+		   draw: function(ease) {
+		      Chart.controllers.line.prototype.draw.call(this, ease);
+
+		      if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+		         var activePoint = this.chart.tooltip._active[0],
+		             ctx = this.chart.ctx,
+		             x = activePoint.tooltipPosition().x,
+		             topY = this.chart.scales['y-axis-0'].top,
+		             bottomY = this.chart.scales['y-axis-0'].bottom;
+
+		         // draw line
+		         ctx.save();
+		         ctx.beginPath();
+		         ctx.moveTo(x, topY);
+		         ctx.lineTo(x, bottomY);
+		         ctx.lineWidth = 2;
+		         ctx.strokeStyle = '#ffffff';
+		         ctx.stroke();
+		         ctx.restore();
+		      }
+		   }
+		});
+
+
 		//the price history chart
 		let ctx = $("#graph");
 		let myChart = new Chart(ctx, {
-		    type: 'line',
+		    type: 'LineWithLine',
 		    data: {
 		        labels: dates,
 		        datasets: [{
@@ -87,9 +114,21 @@ function getHistory(){
 			            	display: true,
 			            	position: 'left',
 			                ticks: {
-			                	suggestedMin: min
+			                	suggestedMin: min,
+			                	fontColor: '#ffffff',
+			                	callback: function(value, index, values) {
+			                        return '$' + value;
+			                    }
 			                }
 			            }
+		            ],
+		            xAxes: [
+		            	{
+		            		display: true,
+		            		ticks: {
+		            			fontColor: '#ffffff'
+		            		}
+		            	}
 		            ]
 		        },
 		        elements: {
@@ -99,6 +138,12 @@ function getHistory(){
 		        },
 		        legend: {
 		        	display: false
+		        },
+		        tooltips: {
+		        	intersect: false,
+		        	mode: 'index',
+		        	displayColors: false,
+
 		        }
 		    }
 		});
