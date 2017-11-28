fb95d9489d66ef9d9fd625cfbe6e64b5b5214968
slyty7 <tyler.j.west.7@gmail.com>
2017-11-27 20:26:59 -0800

    add favicon

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ function getHistory(){
 		      Chart.controllers.line.prototype.draw.call(this, ease);
 
 		      if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
-		         var activePoint = this.chart.tooltip._active[0],
+		         let activePoint = this.chart.tooltip._active[0],
 		             ctx = this.chart.ctx,
 		             x = activePoint.tooltipPosition().x,
 		             topY = this.chart.scales['y-axis-0'].top,
